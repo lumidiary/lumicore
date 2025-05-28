32280aeed84e6a0d5058bdaa419db882c9366d07
JANGWOO JUNG <81564742+Smallt0wn@users.noreply.github.com>
2025-05-29 05:42:30 +0900

    fix : update WebSocket function

--- src/main/java/com/example/lumicore/config/WebSocketConfig.java
+++ src/main/java/com/example/lumicore/config/WebSocketConfig.java
@@ -5,6 +5,7 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.lang.NonNull;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -13,17 +14,18 @@ import lombok.extern.slf4j.Slf4j;
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
+    public void configureMessageBroker(@NonNull MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic");  // 구독 endpoint prefix
         config.setApplicationDestinationPrefixes("/app");  // 메시지 발행 endpoint prefix
     }
 
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        // 코어 엔드포인트만 사용
+    public void registerStompEndpoints(@NonNull StompEndpointRegistry registry) {
         registry.addEndpoint("/core/ws")
-               .setAllowedOriginPatterns("http://localhost:[*]")
-                //.setAllowedOriginPatterns("https://api.lumidiary.com")
-                .withSockJS();
+               .setAllowedOriginPatterns(
+                   "http://localhost:[*]",           // 개발용
+                   "https://api.lumidiary.com"    // 운영용
+               )
+               .withSockJS();
     }
 } 
